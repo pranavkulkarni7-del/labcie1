b53d3343a62d1ef210267bba563efce1293ba3b6
pranavkulkarni7-del <pranavkulkarni834@gmail.com>
2025-10-18 11:55:23 +0530

    added

--- programcie1.py
+++ programcie1.py
@@ -1,3 +1,5 @@
 km = int(input("ente the distance in kiometer"))
 kmtomil = km * 0.6213
+miltokm = km / 0.6213
+print("the distance in kilometer is", miltokm)
 print("the distance in miles is", kmtomil)
